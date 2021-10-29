e7dcdb9f59fc5c35e7a88bbcebf4ddf35173786b
dsereboo <dsereboo@gmail.com>
2021-10-29 14:37:19 +0000

    Graphql resolvers added

--- index.js
+++ index.js
@@ -57,6 +57,106 @@ const typeDefs = gql`
   }
 `;
 
+
+
+//Resolvers
+const resolvers={
+    Query:{
+        posts (parent, args, context, info) {       
+            return Blog.find({})
+                .then (blog => {
+                    let posts=[]
+                    posts=blog
+                   return posts
+                })
+                .catch (err => {
+                    console.error(err)
+                })
+        }
+       
+    },
+
+    Mutation:{
+        createPost(parent, args, context, info){
+            const {title, body, user, id,likes,dislikes, authorID } = args
+            const blogObj = new Blog({
+                title,
+                body,
+                user,
+                id,
+                likes,
+                dislikes,
+                authorID,
+            })
+            blogObj.save()
+            return blogObj
+        },
+
+        updatePost(parent, args, context, info){
+            const{title,body,user, id, likes,dislikes}=args
+            return Blog.find({_id:id}).then(
+                post=>{
+                    if(post.length>0){
+                        post[0].title=title
+                        post[0].body=body
+                        post[0].likes=likes
+                        post[0].dislike=dislikes
+                        post[0].user=user
+                        post[0].save()
+                    }
+                    
+                }).catch(
+                    err=>console.log(err)
+                )
+           
+        },
+
+        async deletePost(parent, args, context, info){
+           const{_id}=args
+           await Blog.findByIdAndDelete(_id)
+           return true
+
+        },
+
+        async likePost(parent, args, context, info){
+           const{_id, likes}=args
+           Blog.findByIdAndUpdate({_id},{$set:{likes:`${likes}+1`}})   
+           return true
+        },
+
+        // async dislikePost(parent, args, context, info){
+        //     const{_id}=args
+        //     Blog.findByIdAndUpdate({_id},{$set:{dislikes:`likes+1`}})   
+        //     return true
+        //  },
+
+        commentPost(parent, args, context, info){
+            const{body, id,postID }=args
+            Blog.find({_id:postID}).then(
+                doc=>{
+                doc[0].comments.push({body:body, id:id})
+                doc[0].save()
+                }
+            )
+            return body
+        },
+
+        async deleteComment(parent, args, context, info){
+            const{body, id,postID }=args
+            await Blog.updateOne({},{
+                $pull:{comments:{id:id}}
+            })
+            // await Blog.find({_id:postID}).then(
+            //     item=>{
+            //        console.log(item[0].comments.filter(item=>item._id!==id))
+            //     }
+            // )
+            return true
+        }
+    }
+    
+}
+
 //Server Instance
 const server= new ApolloServer({typeDefs,resolvers})
 
